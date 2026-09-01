6f7b1b2dbed3daadccbb825afbdb856097339b64
Robert McLeod <git@rob-mc.cloud>
2026-09-01 20:19:13 +1000

    Speculative support for PS 5.1

--- functions/Invoke-Method.ps1
+++ functions/Invoke-Method.ps1
@@ -149,27 +149,38 @@ function Invoke-Method {
         }
     }
 
-    if ($PSVersionTable.PSEdition -eq 'Core') {
-        if ($Context.AuthType -eq 'ApiKey') {
-            # PS Core added native Basic Auth support
-            $params.Credential = $Context.Credential
-            $params.Authentication = 'Basic'
-        } elseif ($Context.AuthType -eq 'ClientCreds') {
+    switch ($Context.AuthType) {
+        'ApiKey' {
+            if ($PSVersionTable.PSEdition -eq 'Core') {
+                # PS Core added native Basic Auth support
+                $params.Credential = $Context.Credential
+                $params.Authentication = 'Basic'
+            } else {
+                # PS Desktop requires manual header creation. Basic auth is only supported by challenge.
+                $raw = '{0}:{1}' -f $Context.Credential.GetNetworkCredential().username, $Context.Credential.GetNetworkCredential().password
+                $bytes = [System.Text.Encoding]::ASCII.GetBytes($raw)
+                $encoded = [Convert]::ToBase64String($bytes)
+
+                $params.Headers.Authorization = "Basic $encoded"
+            }
+        }
+
+        'ClientCreds' {
             if ($null -eq $Context.AccessToken -or $Context.TokenExpiresAt -le [DateTime]::UtcNow) {
                 $null = Request-AccessToken -Context $context
             }
-            $params.Token = $Context.AccessToken
-            $params.Authentication = 'Bearer'
-        } else {
+
+            if ($PSVersionTable.PSEdition -eq 'Core') {
+                $params.Token = $Context.AccessToken
+                $params.Authentication = 'Bearer'
+            } else {
+                $params.Headers.Authorization = 'Bearer {0}' -f ($Context.AccessToken | ConvertFrom-SecureString -AsPlainText)
+            }
+        }
+
+        default {
             throw 'Unknown Auth Type'
         }
-    } else {
-        # PS Desktop requires manual header creation. Basic auth is only supported by challenge.
-        $raw = '{0}:{1}' -f $Context.Credential.GetNetworkCredential().username, $Context.Credential.GetNetworkCredential().password
-        $bytes = [System.Text.Encoding]::ASCII.GetBytes($raw)
-        $encoded = [Convert]::ToBase64String($bytes)
-
-        $params.Headers.Authorization = "Basic $encoded"
     }
 
     if ($PSBoundParameters.ContainsKey('Body')) {
